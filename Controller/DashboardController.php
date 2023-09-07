b4b1ee87d76a979814cef77b5c70d4e11939bdb4
owl-app <owl@pawelkeska.eu>
2023-09-07 11:16:43 +0000

    Psalm fixes

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -1,26 +1,10 @@
 <?php
 
-/*
- * This file is part of the Sylius package.
- *
- * (c) Paweł Jędrzejewski
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 namespace Owl\Bundle\AdminBundle\Controller;
 
 use Owl\Component\Setting\Storage\SettingStorageInterface;
-use Sylius\Bundle\AdminBundle\Provider\StatisticsDataProviderInterface;
-use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
-use Sylius\Component\Core\Dashboard\SalesDataProviderInterface;
-use Sylius\Component\Core\Model\ChannelInterface;
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
@@ -29,7 +13,7 @@ use Twig\Environment;
 final class DashboardController
 {
     /**
-     * @param EngineInterface|Environment $templatingEngine
+     * @param Environment $templatingEngine
      */
     public function __construct(
         private SettingStorageInterface $settingStorage,
